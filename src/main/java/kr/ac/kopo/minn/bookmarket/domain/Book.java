798d6faa5ebe45c55a91a60b4587382f79ae5cc0
kjminn <kjminn2001@gmail.com>
2025-04-30 11:43:07 +0900

    BookMarket의 6장 도서 등록 페이지 완성

--- src/main/java/kr/ac/kopo/minn/bookmarket/domain/Book.java
+++ src/main/java/kr/ac/kopo/minn/bookmarket/domain/Book.java
@@ -1,15 +1,13 @@
 package kr.ac.kopo.minn.bookmarket.domain;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.math.BigDecimal;
 
-@Getter
-@Setter
-@NoArgsConstructor
+@Data
+//@Getter
+//@Setter
+//@NoArgsConstructor
 public class Book {
     private String bookId; //도서번호
     private String name; //도서명
